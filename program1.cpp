848a3fce2d4fa529c232c0326f16a4b1dd4b4d8b
Naman Jain <103673538+NamanJain62@users.noreply.github.com>
2022-07-22 23:26:33 +0530

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+#include <iostream>
+
+int main(){
+    char a= 'n';  //declare character value
+    cout<<"hello World"<<endl; //cout use to display output and "endl" is use to print + new line
+    cout<<a<<endl;
+
+    bool b = true;  //declare boolean value
+    cout<<b<<endl;
+
+    int c= 10;  //declare int value
+    cout<<c<<endl;
+
+    return 0;
+}
