ab184662d8977589c66fbabf2e0b8d5517d449f5
Naman Jain <103673538+NamanJain62@users.noreply.github.com>
2022-07-25 12:09:17 +0530

    Add files via upload

---
+++ program7.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+#include<iostream>
+
+int main(){
+    int i,n=5;
+    for(i=0;i<=n;i++){
+        cout<<i<<endl;
+    }
+
+    while(i<=n){
+            cout<<i<<endl;
+            i++;
+          }
+
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=n);
+}
